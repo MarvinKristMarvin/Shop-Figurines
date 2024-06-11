f9d39e89491536fbc05cee0d7579564b25a61ee2
MarvinKristMarvin <marvin.krist@oclock.school>
2024-06-11 10:11:38 +0200

    setup et partials

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,20 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 const mainController = {
-
   // méthode pour la page d'accueil
   homePage: (request, response) => {
-    const filePath = path.resolve(__dirname + '/../../integration/accueil.html');
+    const filePath = path.resolve(
+      __dirname + "/../../integration/accueil.html"
+    );
     response.sendFile(filePath);
+
+    //response.render("index");
   },
 
   // méthode pour la page article
   articlePage: (request, response) => {
-    const filePath = path.resolve(__dirname + '/../../integration/article.html');
+    const filePath = path.resolve(
+      __dirname + "/../../integration/article.html"
+    );
     response.sendFile(filePath);
-  }
-
+  },
 };
-
 
 module.exports = mainController;
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // Toujours commencer par importer les variables d'environnement !
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 
 // on importe le router
-const router = require('./app/router');
+const router = require("./app/router");
 
 // un peu de config
 const PORT = process.env.PORT || 5000;
 
-
 const app = express();
 
+app.set("views", "./app/views");
+app.set("view engine", "ejs");
+
 // servir les fichiers statiques qui sont dans "integration"
-app.use(express.static('integration'));
+app.use(express.static("integration"));
 
 // routage !
 app.use(router);
-
 
 // on lance le serveur
 app.listen(PORT, () => {
